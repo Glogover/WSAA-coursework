c915c5bb4c4371b06243068958cf81f8cafcd72c
Marcin Kaminski <marcin442228@gmail.com>
2026-02-03 22:32:55 +0000

    HTML

--- mywork/hello.py
+++ mywork/hello.py
@@ -9,4 +9,6 @@ def hello():
 if __name__ == "__main__":
     app.run()
 # To run the Flask application, use the command:
-# python mywork/flask.py    
+# python mywork/flask.py  
+# Then open your web browser and go to http://127.0.0.1:5000
+# You should see "Hello, Flask!" displayed in your browser.  
