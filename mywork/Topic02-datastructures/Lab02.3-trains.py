463ae5cd3e34f00478587e8aef638096ab18b9d3
Marcin Kaminski <marcin442228@gmail.com>
2026-02-24 23:22:38 +0000

    trains XML

---
+++ mywork/Topic02-datastructures/Lab02.3-trains.py
@@ -0,0 +1,16 @@
+# Python program that reads the XML from the URL and prints it out, using minidom.
+# Author: Marcin Kaminski
+
+import requests
+import csv
+from xml.dom.minidom import parseString
+
+url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
+page = requests.get(url)
+doc = parseString(page.content)
+# check it works
+print(doc.toprettyxml()) # output to console, comment this out once you know it works
+
+# if I want to store the xml in a file. You can comment this out later.
+with open("trainxml.xml", "w") as xmlfp:
+    doc.writexml(xmlfp)
