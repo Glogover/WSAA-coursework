100ce33b654b2cab8cbd0b15aaf50939dea1de32
Marcin Kaminski <marcin442228@gmail.com>
2026-02-24 23:37:52 +0000

    trains.py

--- mywork/Topic02-datastructures/Lab02.3-trains.py
+++ mywork/Topic02-datastructures/Lab02.3-trains.py
@@ -9,8 +9,19 @@ url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
 page = requests.get(url)
 doc = parseString(page.content)
 # check it works
-print(doc.toprettyxml()) # output to console, comment this out once you know it works
+#print(doc.toprettyxml()) # output to console, comment this out once you know it works
 
 # if I want to store the xml in a file. You can comment this out later.
 with open("trainxml.xml", "w") as xmlfp:
     doc.writexml(xmlfp)
+
+objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
+for objTrainPositionsNode in objTrainPositionsNodes:
+    #traincodeNode = objTrainPositionsNode.getElementsByTagName("TrainCode").item(0)
+    #traincode = traincodeNode.firstChild.nodeValue.strip()
+    #print(traincode)
+    trainlatitudeNode = objTrainPositionsNode.getElementsByTagName("TrainLatitude").item(0)
+    trainlatitude = trainlatitudeNode.firstChild.nodeValue.strip()
+    print(trainlatitude)
+
+
