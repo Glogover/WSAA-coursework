b26390fbdf1d463ae4a93495de30447d21151950
Marcin Kaminski <marcin442228@gmail.com>
2026-02-25 00:50:15 +0000

    trains.py

--- mywork/Topic02-datastructures/Lab02.3-trains.py
+++ mywork/Topic02-datastructures/Lab02.3-trains.py
@@ -5,6 +5,15 @@ import requests
 import csv
 from xml.dom.minidom import parseString
 
+retrieveTags=['TrainStatus',
+            'TrainLatitude',
+            'TrainLongitude',
+            'TrainCode',
+            'TrainDate',
+            'PublicMessage',
+            'Direction'
+            ]
+
 url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
 page = requests.get(url)
 doc = parseString(page.content)
@@ -15,13 +24,36 @@ doc = parseString(page.content)
 with open("trainxml.xml", "w") as xmlfp:
     doc.writexml(xmlfp)
 
-objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
-for objTrainPositionsNode in objTrainPositionsNodes:
-    #traincodeNode = objTrainPositionsNode.getElementsByTagName("TrainCode").item(0)
-    #traincode = traincodeNode.firstChild.nodeValue.strip()
-    #print(traincode)
-    trainlatitudeNode = objTrainPositionsNode.getElementsByTagName("TrainLatitude").item(0)
-    trainlatitude = trainlatitudeNode.firstChild.nodeValue.strip()
-    print(trainlatitude)
+# I had an issue with blank lines in the file so found solution at
+# https://stackoverflow.com/questions/3348460/csv-file-written-with-python-has-blank-lines-between-each-row
+# adding the newline= '' parameter
+with  open('week03_train.csv', mode='w', newline='') as train_file:
+    train_writer = csv.writer(train_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
+
+    for objTrainPositionsNode in objTrainPositionsNodes:
+       #traincodeNode = objTrainPositionsNode.getElementsByTagName("TrainCode").item(0)
+       #traincode = traincodeNode.firstChild.nodeValue.strip()
+       #print(traincode)
+
+       #trainlatitudeNode = objTrainPositionsNode.getElementsByTagName("TrainLatitude").item(0)
+       #trainlatitude = trainlatitudeNode.firstChild.nodeValue.strip()
+       #print(trainlatitude)
+       
+       # now lets get everything
+       dataList = []
+
+
+       for retrieveTag in retrieveTags:
+            datanode = objTrainPositionsNode.getElementsByTagName(retrieveTag).item(0)
+            dataList.append(datanode.firstChild.nodeValue.strip())
+
+        # instead of printing this you could output to another format
+        #print (dataList)
+        # for example a CSV file 
+    #train_code = dataList[3] 
+    #if train_code.startswith("D"): # only write out DART trains
+            train_writer.writerow(dataList)
 
 
