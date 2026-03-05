5a83e435fedc17c7c649e6c0455ce25dad52b59e
Marcin Kaminski <marcin442228@gmail.com>
2026-03-05 19:53:12 +0000

    cso DAO

--- mywork/Topic04-apis/csodao_2.py
+++ mywork/Topic04-apis/csodao_2.py
@@ -15,7 +15,9 @@ def getAll(dataset):
     return response.json()
 
 def getFormattedAsFile(dataset):
-    pass
+    with open("cso-formatted.json", "wt") as fp:
+        print(json.dumps(getFormatted(dataset)), file = fp)
+
 
 def getFormatted(dataset):
     data = getAll(dataset)
@@ -24,35 +26,47 @@ def getFormatted(dataset):
     dimensions = data["dimension"]
     sizes = data["size"]
     valuecount = 0
-    result = 0
+    result = {}
+  
 
     for dim0 in range(0, sizes[0]): # loop through the first dimension
         currentId = ids[0]
         index = dimensions[currentId]["category"]["index"][dim0]
-        label = dimensions[currentId]["category"]["label"][index]
-        print(label)
+        label0 = dimensions[currentId]["category"]["label"][index]
+        result[label0] = {}
+      
+        #print(label0)
+        
         for dim1 in range(0, sizes[1]): # loop through the second dimension
             currentId = ids[1]
             index = dimensions[currentId]["category"]["index"][dim1]
-            label = dimensions[currentId]["category"]["label"][index]
-            print("\t",label)
+            label1 = dimensions[currentId]["category"]["label"][index]
+            result[label0][label1] = {}
+           
+            #print("\t",label1)
+            
             for dim2 in range(0, sizes[2]): # loop through the third dimension
                 currentId = ids[2]
                 index = dimensions[currentId]["category"]["index"][dim2]
-                label = dimensions[currentId]["category"]["label"][index]
-                print("\t\t",label)
+                label2 = dimensions[currentId]["category"]["label"][index]
+                result[label0][label1][label2] = {}
+               
+                #print("\t\t",label2)
+                
                 for dim3 in range(0, sizes[3]): # loop through the fourth dimension
                     currentId = ids[3]
                     index = dimensions[currentId]["category"]["index"][dim3]
-                    label = dimensions[currentId]["category"]["label"][index]
-                    print("\t\t\t",label, " ", values[valuecount])
+                    label3 = dimensions[currentId]["category"]["label"][index]
+                    #print("\t\t\t",label, " ", values[valuecount])
+                    result[label0][label1][label2][label3] = values[valuecount]   
                     valuecount+=1
 
-        
+    print(result)
+    return result
 
 if __name__ == "__main__":
     #getAllasFile("FP001")
-    getFormatted("FP001")
+    getFormattedAsFile("FP001")
 
 
 
