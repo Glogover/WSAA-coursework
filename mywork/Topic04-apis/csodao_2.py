50082e9d3d1797c25f6964bba2ddddd8cde53b39
Marcin Kaminski <marcin442228@gmail.com>
2026-03-05 12:25:26 +0000

    cso.json

--- mywork/Topic04-apis/csodao_2.py
+++ mywork/Topic04-apis/csodao_2.py
@@ -2,18 +2,24 @@ import requests
 import json
 
 
-url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FP001/JSON-stat/2.0/en"
+urlBeginning = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/"
+urlEnd = "/JSON-stat/2.0/en"
 
-def getAll():
+def getAllasFile(dataset):
+    with open("cso.json", "wt") as fp:
+        print(json.dumps(getAll(dataset)), file = fp)
+
+def getAll(dataset):
+    url = urlBeginning + dataset + urlEnd
     response = requests.get(url)
     return response.json()
 
 
-
 if __name__ == "__main__":
-    with open("cso.json", "wt") as fp:
-        print(json.dumps(getAll()), file = fp)
-        
+    getAllasFile("FP001")
+
+    
+
 
 
 
