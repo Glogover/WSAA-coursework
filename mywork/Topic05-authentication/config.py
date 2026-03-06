a7227a00c5a8bad5a183e19a7882e38ac91398bf
Marcin Kaminski <marcin442228@gmail.com>
2026-03-06 21:02:37 +0000

    html to pdf demo

---
+++ mywork/Topic05-authentication/config.py
@@ -0,0 +1,3 @@
+apikeys = {
+    "htmltopdfkey":"0pzXjo64uAgtiAR3PRWWzx8sdtpCMO0J9pos8j1WHrjQNCoBmhA7ytwfwopAN2eW"
+}
--- mywork/Topic05-authentication/htmltopdfdemo.py
+++ mywork/Topic05-authentication/htmltopdfdemo.py
@@ -4,14 +4,28 @@
 
 import requests # for making HTTP requests
 import urllib.parse # for URL encoding
+from config import apikeys as cfg # we import the API keys from a separate config file for better security and organization
 
-targetUrl = "https://en.wikipedia.org" # this is the web page we want to convert to PDF
+#targetUrl = "https://en.wikipedia.org" # this is the web page we want to convert to PDF
+targetUrl = "https://example.com/" # you can change this to any web page you want, but make sure it is a valid URL and that the API can access it (some websites may block requests from certain sources)
 
-apikiey = "LXVsRthF6WocSPJXxUytgrkpM0x8kONlsdm5lnPry5fsdUfF8WFWI0Y9OG3kLASJ" # you can get a free API key from https://html2pdf.app/ (limited to 100 conversions per month)
-apiurl = "https://api.html2pdf.app/v1/generate" # this is the API endpoint for generating PDF files from web pages
+apikey = cfg["htmltopdfkey"] # we get the API key from the config file
+# you can get a free API key from https://html2pdf.app/ (limited to 100 conversions per month and 1 MB file size, but you can upgrade to a paid plan for more conversions and larger file sizes)
 
-params = {"html": targetUrl, "apiKey": apikiey} # these are the parameters we need to send to the API: the URL of the web page and our API key
-parsedParams = urllib.parse.urlencode(params) # we need to URL-encode the parameters to make sure they are properly formatted for the HTTP request
+apiurl = "https://api.html2pdf.app/v1/generate"  # this is the API endpoint for generating PDF files from web pages
 
-requestUrl = apiurl + "?" + parsedParams # this is the full URL we will use to make the HTTP request to the API, including the parameters
+params = {"html": targetUrl, "apiKey": apikey} # these are the parameters we need to send to the API: the URL of the web page and our API key
+parsedparams = urllib.parse.urlencode(params) # we need to URL-encode the parameters to make sure they are properly formatted for the HTTP request
+
+requestUrl = apiurl + "?" + parsedparams # this is the full URL we will use to make the HTTP request to the API, including the parameters
 print(requestUrl) #print the request URL to the console for debugging purposes
+
+response = requests.get(requestUrl) # make the HTTP GET request to the API and store the response in a variable
+
+print(response.status_code) # print the HTTP status code of the response to the console for debugging purposes
+
+result = response.content # the content of the response is the PDF file in binary format, so we store it in a variable
+
+with open ("document.pdf", "wb") as handler: # we open a new file called "document.pdf" in binary write mode
+    handler.write(result) # we write the content of the response (the PDF file) to the new file
+
