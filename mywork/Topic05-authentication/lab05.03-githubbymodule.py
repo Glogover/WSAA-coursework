a7c9dddd6286f87a4050d87b800b7e859cb5f0d9
Marcin Kaminski <marcin442228@gmail..com>
2026-03-26 21:50:31 +0000

    lab05

--- mywork/Topic05-authentication/lab05.03-githubbymodule.py
+++ mywork/Topic05-authentication/lab05.03-githubbymodule.py
@@ -1,4 +1,5 @@
 from github import Github
+import requests
 from config2 import apikeys as cfg
 
 apikey = cfg["githubkey"]
@@ -7,7 +8,25 @@ apikey = cfg["githubkey"]
 g = Github(apikey)
 
 repo = g.get_repo("Glogover/aprivateone")
-print(repo.clone_url)
+#print(repo.clone_url)
 
-for repo in g.get_user().get_repos():
-    print(repo.name)
+fileInfo = repo.get_contents("test.txt")
+urlOfFile = fileInfo.download_url
+#print (urlOfFile)
+
+#for repo in g.get_user().get_repos():
+    #print(repo.name)
+
+response = requests.get(urlOfFile)
+contentOfFile = response.text
+print (contentOfFile)
+
+newContents = contentOfFile + " more stuff \n"
+print (newContents)
+
+gitHubResponse=repo.update_file(fileInfo.path,"updated by prog",
+newContents,fileInfo.sha)
+print (gitHubResponse)
+
+
+
