fcf28c592c30a3793b422f0d32d7be683a8e8dc2
Marcin Kaminski <marcin442228@gmail..com>
2026-03-28 16:54:52 +0000

    rest-server.py

---
+++ mywork/Topic06-implementation/rest-server.py
@@ -0,0 +1,21 @@
+# REST server example using Flask
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return "Hello world"
+
+@app.route("/blah2")
+
+def blah():
+    return "this is blah2"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
