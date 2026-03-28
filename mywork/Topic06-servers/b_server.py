01e67c0ff1205965172e95921a30e83986778d84
Marcin Kaminski <marcin442228@gmail..com>
2026-03-28 12:25:02 +0000

    b_server.py

--- mywork/Topic06-servers/b_server.py
+++ mywork/Topic06-servers/b_server.py
@@ -1,9 +1,35 @@
-from flask import Flask
+from flask import Flask, url_for, redirect
 
 app = Flask(__name__, static_url_path='', static_folder='staticpages')
 
 # mapping
+@app.route("/")
+def index():
+    return "<h1>hi there mom</h1>" # this is the response to the request.HTML is just text, but the browser will render it as HTML.
 
+@app.route('/users', methods=['GET'])
+def get_users():
+    return "getting all users"
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    return "creating a new user"
+
+@app.route('/users', methods=['PUT'])
+def update_user():
+    return "updating a user"
+
+@app.route('/invalid', methods=['GET'])
+def testingredirect():
+    return redirect(url_for(index)) # this will redirect to the index page
+
+@app.route('/users/<username>', methods=['GET'])
+def get_user_byname(username):
+    return f"hello {username}"
+
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user_byid(id):
+    return f"your id is {id}"
 
 if __name__ == "__main__":
     app.run(debug=True)
