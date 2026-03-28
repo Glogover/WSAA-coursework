288651f4691827c0b0ff865f15da0274525d1a1a
Marcin Kaminski <marcin442228@gmail..com>
2026-03-28 16:10:09 +0000

    c_server.py

--- mywork/Topic06-servers/c_server.py
+++ mywork/Topic06-servers/c_server.py
@@ -9,8 +9,23 @@ def index():
 @app.route("/inquery")
 def inquery():
     name = request.args.get("name")
-    return name
-    #return request.args
+    #return name
+    return request.args
+
+@app.route("/inbody", methods=["POST"])
+def inbody():
+    name = request.json["name"]
+    age = request.json["age"]
+    #print (request.json)
+    return f"you are {name} and aged {age} {type(age)}"
+
+@app.route("/user")
+def getuser():
+    user = {
+        'name':"andrew",
+        'age':21
+    }
+    return jsonify(user)
 
 
 
